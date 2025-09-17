1caa592bfbb6f46a05a5cc08734da2eac2e2e011
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-09-17 13:18:33 +0300

    Fix file inclusion for `lint` script

--- packages/utils/eslint.config.mjs
+++ packages/utils/eslint.config.mjs
@@ -13,7 +13,7 @@ export default [
         ...globals.jest,
       },
       parserOptions: {
-        project: "./tsconfig.json",
+        project: "./tsconfig.eslint.json",
       },
     },
   },
